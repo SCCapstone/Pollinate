b81d61fe46bfb5a356212e97b24a95d0aaebdb04
Bailey Metz <bametz23@gmail.com>
2018-10-24 18:12:38 -0400

    Bailey Edits

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -4,7 +4,20 @@ import './style.css';
 class About extends Component {
   render() {
     return (
-      <h1>About</h1>
+      <div>
+      <h1>About Tab Works!</h1>
+      
+      <h2>Testing skeleton Pollinate application.</h2>
+      
+      <h4> Successfully able to clone repo, edit and run on local machine, and push to GitHub. </h4>
+      <i>Bailey Metz </i>
+	
+	<h2> _________________________________________________________________________________ </h2>
+
+
+      </div>
+
+
     )
   }
 }
