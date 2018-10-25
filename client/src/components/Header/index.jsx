13686a45240b4fcd81213d37be8d135f00cb5282
Michael Afrin <mkafrin@gmail.com>
2018-10-24 23:53:02 -0400

    Fixed routing to use <Link> instead of <a>

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './style.css';
 
 class Header extends Component {
   render() {
     return (
       <div id={'navBar'}>
-          <a href="/">Home</a>
-          <a href="/login">Login</a>
-          <a href="/profile">Profile</a>
-          <a href="/product">Product</a>
-          <a href="/category">Category</a>
-          <a href="/about">About</a>
+          <Link to="/">Home</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/profile">Profile</Link>
+          <Link to="/product">Product</Link>
+          <Link to="/category">Category</Link>
+          <Link to="/about">About</Link>
       </div>
     )
   }
