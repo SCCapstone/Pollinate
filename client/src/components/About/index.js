cc31144d82fbd863c3ea9d93ed072ce8a8832f1e
Michael Afrin <mkafrin@gmail.com>
2018-10-24 20:25:49 -0400

    Updated about page, added package-lock.json to gitignore

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -4,27 +4,30 @@ import './style.css';
 class About extends Component {
   render() {
     return (
-      <div>
-      
+        <div>
 
-    <h1>Testing Skeleton Pollinate Application</h1>
+          <h1>Testing Skeleton Pollinate Application</h1>
 
-      <h2> _________________________________________________________________________________ </h2>
+          <h2> _________________________________________________________________________________ </h2>
 
-      <h4> Successfully able to clone repo, edit and run on local machine, and push to GitHub. </h4>
-      Here is a small change for other members to git pull.
-      
-      <br /> <i>Bailey Metz </i>
+          <h4> Successfully able to clone repo, edit and run on local machine, and push to GitHub. </h4>
+          Here is a small change for other members to git pull.
 
-	<h2> _________________________________________________________________________________ </h2>
+          <br/> <i>Bailey Metz </i>
+
+          <h2> _________________________________________________________________________________ </h2>
 
           <h2>About Jacob's Change</h2>
           <h3> Spooky scary skeleton</h3>
           <i>Jacob Miller</i>
-      </div>
 
+          <h2> _________________________________________________________________________________ </h2>
 
+          <h2>About Michael's Change</h2>
+          <h3>Added the initial commit for the skeleton</h3>
+          <i>Michael Afrin</i>
 
+        </div>
     )
   }
 }
