a122d58c272d2c5394004a497a6cbdaf5b5f36d4
Christopher 'Jacob' Miller <cjmiller1337@gmail.com>
2018-11-11 18:26:05 -0500

    Implemented barebones login page with account storage

--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -1,15 +1,65 @@
 import React, { Component } from 'react';
 import './style.css';
+//Placeholder importing using firebase
+// import { firebaseApp } from '../firebase';
 
 class Login extends Component {
-  render() {
-    return (
-      <div>
-          <h1>Login</h1>
-          <img src="https://camo.githubusercontent.com/5d01511a026a31b654639d1f48065cf226c17eb4/68747470733a2f2f692e696d6775722e636f6d2f3377706e494f6d2e6a7067" alt=""/>
-      </div>
-    )
-  }
+    //Creating a state for our react class
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            error: {
+                message: ''
+            }
+        }
+    }
+
+    //signUp helper method
+    Login() {
+        console.log('this.state', this.state);
+        //Placeholder firebase utilization
+        /*const { email, password } = this.state;
+        firebaseApp.auth().createUserWithEmailAndPassword(email, password)
+            .catch(error => {
+                console.log('error', error);
+                this.setState({error})
+            })*/
+    }
+
+    render() {
+        return (
+            <div className="form-inline" style={{margin: '5%'}}>
+                <h2>Welcome Back</h2>
+                <div className="form-group">
+                    <input
+                        className="form-control"
+                        type="text"
+                        style={{marginRight: '5px'}}
+                        placeholder="email"
+                        onChange={event => this.setState({email: event.target.value})}
+                    />
+                    <input
+                        className="form-control"
+                        type="password"
+                        style={{marginRight: '5px'}}
+                        placeholder="password"
+                        onChange={event => this.setState({password: event.target.value})}
+                    />
+                    <button
+                        className="btn btn-primary"
+                        type="button"
+                        //Attach helper method to click of button
+                        onClick={() => this.Login()}
+                    >
+                        Log In
+                    </button>
+                </div>
+                <div>{this.state.error.message}</div>
+            </div>
+        )
+    }
 }
 
 export default Login;
