ad9a81cb9b58eac77901475f14c45f449843a2c2
Bailey Metz <bametz23@gmail.com>
2018-11-25 19:39:04 -0500

    Small Test

--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -6,7 +6,8 @@ class Home extends Component {
     return (
         <div>
             <h1>Home</h1>
-            <img src="https://camo.githubusercontent.com/7d56c7ba725a6a4f5743c39526606ccce7c904dd/68747470733a2f2f692e696d6775722e636f6d2f754e53506b6c552e6a7067" alt=""/>
+            // src="https://camo.githubusercontent.com/7d56c7ba725a6a4f5743c39526606ccce7c904dd/68747470733a2f2f692e696d6775722e636f6d2f754e53506b6c552e6a7067" alt=""/>
+
         </div>
     )
   }
