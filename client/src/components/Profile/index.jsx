f22969c573e16b59a22516ea0ddb28d81bd236e7
Christopher 'Jacob' Miller <cjmiller1337@gmail.com>
2018-11-25 22:54:56 -0500

    Added dummy data version of front-end for profile

--- client/src/components/Profile/index.jsx
+++ client/src/components/Profile/index.jsx
@@ -1,15 +1,77 @@
 import React, { Component } from 'react';
 import './style.css';
 
-class Profile extends Component {
-  render() {
+class App extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            person: {
+                name: 'Jacob Miller',
+                location: 'Columbia, SC',
+                biography: '22 year old student living in Columbia. Originally from Phoenix, Arizona. Love to hike and travel.',
+                accountType: 'Founder',
+                joinDate: '11/25/18',
+                favoriteCategory: 'Photography',
+                dealsPosted: '164'
+            },
+            image: {
+                link: 'https://i.imgur.com/iy4Eo4j.jpg',
+                alt: ''
+            },
+            quote: {
+                content: 'Not all those who wander are lost',
+                source: 'J. R. R. Tolkien'
+            }
+
+        };
+    }
+
+    render() {
+        return(
+            <div className="App">
+                <Image src={this.state.image.link} />
+                <Profile person={this.state.person} quote={this.state.quote} />
+            </div>
+        );
+    }
+}
+function Profile(props){
     return (
-      <div>
-          <h1>Profile</h1>
-          <img src="https://camo.githubusercontent.com/12a67e2b4efc46f9e67ead3b3eb52ffc0e6033d9/68747470733a2f2f692e696d6775722e636f6d2f413261787476622e706e67" alt=""/>
-      </div>
-    )
-  }
+        <div id="user-profile">
+        <div className="Profile">
+            {/*Until I can get the constructor to work img src is here*/}
+            <div id="avatar">
+            <img src="https://i.imgur.com/iy4Eo4j.jpg" alt="" width="200" height="200"/>
+            </div>
+            <h2 className="Name">{props.person.name}</h2>
+            <h3 className="Location">{props.person.location}</h3>
+            <hr />
+            <div id="bottom">
+                <h4>Biography</h4>
+            <p className="Bio">{props.person.biography}</p>
+            <div className="Quote">
+                <blockquote>&ldquo; {props.quote.content} &rdquo;</blockquote>
+                <div className="byline">&mdash; {props.quote.source}</div>
+            </div>
+            </div>
+
+        </div>
+            <hr />
+            <b>Account Type</b><p className="accountType">{props.person.accountType}</p>
+
+            <b>Join Date</b><p className="joinDate">{props.person.joinDate}</p>
+
+            <b>Favorite Category</b><p className="favoriteCategory">{props.person.favoriteCategory}</p>
+
+            <hr />
+            <b>Deals Posted</b><p className="dealsPosted">{props.person.dealsPosted}</p>
+        </div>
+    );
 }
 
-export default Profile;
+function Image(props){
+    return (
+        <div className="Image" style={{backgroundImage: 'url(' + props.src + ')'}}></div>
+    );
+}
+export default App;
