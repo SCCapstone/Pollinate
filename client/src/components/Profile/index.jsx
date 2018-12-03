8621dea552e10d3f954e0446603d712efbf04205
Michael Afrin <mkafrin@gmail.com>
2018-12-03 13:26:15 -0500

    connected profile page to backend

--- client/src/components/Profile/index.jsx
+++ client/src/components/Profile/index.jsx
@@ -2,76 +2,69 @@ import React, { Component } from 'react';
 import './style.css';
 
 class App extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            person: {
-                name: 'Jacob Miller',
-                location: 'Columbia, SC',
-                biography: '22 year old student living in Columbia. Originally from Phoenix, Arizona. Love to hike and travel.',
-                accountType: 'Founder',
-                joinDate: '11/25/18',
-                favoriteCategory: 'Photography',
-                dealsPosted: '164'
-            },
-            image: {
-                link: 'https://i.imgur.com/iy4Eo4j.jpg',
-                alt: ''
-            },
-            quote: {
-                content: 'Not all those who wander are lost',
-                source: 'J. R. R. Tolkien'
-            }
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: {
+        link: 'https://i.imgur.com/iy4Eo4j.jpg',
+        alt: ''
+      },
+      quote: {
+        content: 'Not all those who wander are lost',
+        source: 'J. R. R. Tolkien'
+      }
 
-        };
-    }
+    };
+  }
 
-    render() {
-        return(
-            <div className="App">
-                <Image src={this.state.image.link} />
-                <Profile person={this.state.person} quote={this.state.quote} />
-            </div>
-        );
-    }
+  componentDidMount() {
+    fetch("http://localhost:8080/users/me")
+        .then(res => res.json())
+        .then(person => this.setState({person}))
+  }
+
+  render() {
+    console.log(this.state);
+    return(
+        <div className="App">
+          <Profile person={this.state.person || {}} quote={this.state.quote} image={this.state.image}/>
+        </div>
+    );
+  }
 }
 function Profile(props){
-    return (
-        <div id="user-profile">
+  return (
+      <div id="user-profile">
         <div className="Profile">
-            {/*Until I can get the constructor to work img src is here*/}
-            <div id="avatar">
-            <img src="https://i.imgur.com/iy4Eo4j.jpg" alt="" width="200" height="200"/>
-            </div>
-            <h2 className="Name">{props.person.name}</h2>
-            <h3 className="Location">{props.person.location}</h3>
-            <hr />
-            <div id="bottom">
-                <h4>Biography</h4>
+          {/*Until I can get the constructor to work img src is here*/}
+          <div id="avatar">
+            <img id="profilePic" src="https://i.imgur.com/iy4Eo4j.jpg" alt="" width="200" height="200"/>
+          </div>
+          <h2 className="Name">{props.person.name}</h2>
+          <h3 className="Location">{props.person.location}</h3>
+          <hr />
+          <div id="bottom">
+            <h4>Biography</h4>
             <p className="Bio">{props.person.biography}</p>
             <div className="Quote">
-                <blockquote>&ldquo; {props.quote.content} &rdquo;</blockquote>
-                <div className="byline">&mdash; {props.quote.source}</div>
+              <blockquote>&ldquo; {props.quote.content} &rdquo;</blockquote>
+              <div className="byline">&mdash; {props.quote.source}</div>
             </div>
-            </div>
+          </div>
 
         </div>
-            <hr />
-            <b>Account Type</b><p className="accountType">{props.person.accountType}</p>
+        <hr />
+        <b>Account Type</b><p className="accountType">{props.person.accountType}</p>
 
-            <b>Join Date</b><p className="joinDate">{props.person.joinDate}</p>
+        <b>Join Date</b><p className="joinDate">{props.person.joinDate}</p>
 
-            <b>Favorite Category</b><p className="favoriteCategory">{props.person.favoriteCategory}</p>
+        {/*<b>Favorite Category</b><p className="favoriteCategory">{props.person.favoriteCategory}</p>*/}
 
-            <hr />
-            <b>Deals Posted</b><p className="dealsPosted">{props.person.dealsPosted}</p>
-        </div>
-    );
+        <hr />
+        <b>Deals Posted</b><p className="dealsPosted">{props.person.dealsPosted}</p>
+      </div>
+  );
 }
 
-function Image(props){
-    return (
-        <div className="Image" style={{backgroundImage: 'url(' + props.src + ')'}}></div>
-    );
-}
+
 export default App;
