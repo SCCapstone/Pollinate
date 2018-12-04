6e156f71a5bd76b564bea8f07ed2a7db00fbf350
Bailey Metz <bametz23@gmail.com>
2018-12-03 19:55:42 -0500

    added home_page data fetch

--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -2,46 +2,54 @@ import React, { Component } from 'react';
 import './style.css';
 
 class Home extends Component {
-  render() {
 
+    constructor(props){
+        super(props);
+        this.state = {
+            posts: []
+        }
+    }
+
+    componentDidMount() {
+        fetch("http://localhost:8080/api/products")
+            .then(res => res.json())
+            .then(posts => this.setState({posts}))
+
+    }
+
+
+    render() {
+      var posts = this.state.posts.map(post => {
+          return (
+              <Post post={post}/>
+          )
+      })
       return (
 
-        <div className="text-center">
+        <div>
 
             <h1> Popular Deals </h1>
 
             <p> <button onClick=""> Post A Deal </button> </p>
 
             <div>
-
-
-                <div className="filters">
-                    <img src="https://static.shoplightspeed.com/shops/619494/files/009061409/800x1024x2/brushy-logo-mug-white-circle-keys-logo-coffee-mug.jpg" width="200" height="200"/>
-                    <h3>Coffee Mug</h3> <h2>$3.99</h2>
-
-                    <img src="https://c.static-nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/kod3bxfy7vx99bca26cb/elite-competition-8p-basketball-721MyM.jpg" width="200" height="200"/>
-                    <h3>Basketball</h3> <h2>$29.99</h2>
-
-                    <img src="https://www.gorillacasestore.com/wp-content/uploads/2017/07/waterproof-iphone-7-case-red-1-800x800.jpg" width="200" height="200"/>
-                    <h3>iPhone 7 Case</h3> <h2>$10.99</h2>
-
-                </div>
-
-                <div className="images">
-                    <img src=""/>
-                </div>
+                {posts}
             </div>
 
         </div>
-
-
-
-
-
 
 
     )
   }
 }
 
+function Post(props)
+{
+    return(
+        <div className="itemBorder" onClick={this.props.history.push("/product/"+ props.post.id)}>
+            <img src={props.post.imageUrl}/>
+            <h3>{props.post.name}</h3> <h2>{props.post.price}</h2>
+        </div>
+    )
+}
 export default Home;
