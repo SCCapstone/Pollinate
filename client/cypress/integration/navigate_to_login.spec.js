cf40fdf280028ddc4104f14bb4feff8904cf728d
Michael Afrin <mkafrin@gmail.com>
2019-02-03 20:05:06 -0500

    added cypress e2e testing framework and first behavioral test

---
+++ client/cypress/integration/navigate_to_login.spec.js
@@ -0,0 +1,9 @@
+describe('My First Test', function () {
+  it('Clicks login button to navigate to login page', function () {
+    cy.viewport(1280, 720);
+    cy.visit("/");
+    cy.get("a[href='/login']").click();
+    cy.url().should('include', '/login');
+    cy.get('div#LoginPage').should('exist');
+  })
+});
