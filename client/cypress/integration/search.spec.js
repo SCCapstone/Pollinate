2ceaa058e63dd596d73ef1c21f9d6cfe2f30721d
Blake Pathammavong <bbp@email.sc.edu>
2019-02-18 20:47:56 -0500

    Behavioral Test has been created for the SearchImplementation

--- client/cypress/integration/search.spec.js
+++ client/cypress/integration/search.spec.js
@@ -1,7 +1,9 @@
 describe('Search', function () {
-    it('Searches through search function trying to find laptop', function () {
+    it('Searches through search function trying to find drill', function () {
         cy.viewport(1280, 720);
         cy.visit("/");
-
+        cy.get("#headerSearch").type('Drill{enter}');
+        cy.url().should('include', '/search?keyword=Drill');
+        cy.get('.post').contains('Drill').its('length').should('be.gte',1);
     })
 });
