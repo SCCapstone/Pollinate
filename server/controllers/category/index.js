f14984d4232be25c136828d8d063e4e75f75107a
Michael Afrin <mkafrin@gmail.com>
2019-04-05 05:23:25 -0400

    Fixed category controller to only send unexpired posts to the front-end

--- server/controllers/category/index.js
+++ server/controllers/category/index.js
@@ -4,7 +4,7 @@ const db = require('../../utils/database');
 //gets all posts
 exports.getAll = function(req, res) {
     var category = req.params.category;
-    db.query("SELECT * FROM posts WHERE category=?", [category], function(err, result, fields) {
+    db.query("SELECT * FROM all_posts WHERE category=?", [category], function(err, result, fields) {
         if(err) return res.status(500).end();
         res.status(200).send(result);
     });
