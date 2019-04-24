5e876806ce8fc9e236c3f3a7d1853f4521851c9d
Christopher 'Jacob' Miller <cjmiller1337@gmail.com>
2019-04-24 08:28:09 -0400

    Pictures added

--- client/src/pages/About/index.jsx
+++ client/src/pages/About/index.jsx
@@ -1,5 +1,10 @@
 import React, {Component} from 'react';
 import logo from './logo.png';
+import Jacob from './Jacob.jpg';
+import Bailey from './Bailey.jpg';
+import Blake from './Blake.jpg';
+import Justin from './Justin.jpg';
+import Mike from './Mike.jpg';
 import './style.css';
 
 class About extends Component {
@@ -11,6 +16,11 @@ class About extends Component {
           <p> Pollinate's mission is to organize the internet's best sales and deals, all while making it universally convenient </p>
             </div>
             <h4 className = "founders">Meet the Founders</h4>
+                <img className = "foundee" src={Jacob}/>
+            <img className = "foundee" src={Bailey}/>
+            <img className = "foundee" src={Blake}/>
+            <img className = "foundee" src={Justin}/>
+            <img className = "foundee" src={Mike}/>
         </div>
     )
   }
