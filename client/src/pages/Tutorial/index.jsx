7ba5eee13b655a703459846b6cf00f7b090ffc3f
Blake Pathammavong <bbp@email.sc.edu>
2019-04-24 22:19:13 -0400

    Co-authored-by: Bailey Metz <32320395+bametz23@users.noreply.github.com> Co-authored-by: Christopher "Jacob" Miller <32064300+jacmiller@users.noreply.github.com> Co-authored-by: Justin Morris <15788366+justin-morris@users.noreply.github.com>

--- client/src/pages/Tutorial/index.jsx
+++ client/src/pages/Tutorial/index.jsx
@@ -1,11 +1,56 @@
 import React, {Component} from 'react';
+import HomePage from './HomePage.png';
+import CategoryPage from './CategoryPage.png';
+import ItemPage from './ItemPage.png';
+import LoginPage from './LoginPage.png';
+import NewPostPage from './NewPostPage.png';
+import ProfilePage from './ProfilePage.png';
+import SearchPage from './SearchPage.png';
+import SignUpPage from './SignUpPage.png';
 import './style.css';
 
 class Tutorial extends Component {
   render() {
     return (
         <div>
-          <h1> Tutorial -- To Be Developed </h1>
+            <h1 className="headline">Video Tutorial</h1>
+            <div className="headline">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/rQTtZSnJeoc" frameBorder="0"
+                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen></iframe>
+            </div>
+            <h1 className="scrnmessage">Pollinate Home Page</h1>
+            <div className="screenshot">
+                <img className="screenshot" src={HomePage}/>
+            </div>
+            <h2 className="scrnmessage">Pollinate Category Page</h2>
+            <div className="screenshot">
+                <img className="screenshot" src={CategoryPage}/>
+            </div>
+            <h3 className="scrnmessage">Pollinate Item Page</h3>
+            <div className="screenshot">
+                <img className="screenshot" src={ItemPage}/>
+            </div>
+            <h4 className="scrnmessage">Pollinate Login Page</h4>
+            <div className="screenshot">
+                <img className="screenshot" src={LoginPage}/>
+            </div>
+            <h4 className="scrnmessage">Pollinate New Post Page</h4>
+            <div className="screenshot">
+                <img className="signup" src={NewPostPage}/>
+            </div>
+            <h5 className="scrnmessage">Pollinate Profile Page</h5>
+            <div className="screenshot">
+                <img className="screenshot" src={ProfilePage}/>
+            </div>
+            <h6 className="scrnmessage">Pollinate Search Page</h6>
+            <div className="screenshot">
+                <img className="screenshot" src={SearchPage}/>
+            </div>
+            <h4 className="scrnmessage">Pollinate SignUp Page</h4>
+            <div className="screenshot">
+                <img className="signup" src={SignUpPage}/>
+            </div>
         </div>
     )
   }
