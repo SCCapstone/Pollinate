db15cd4bdc0ffe27d66bbbc4a8ea949735139d93
Michael Afrin <mkafrin@gmail.com>
2019-04-25 00:03:10 -0400

    cleaned up tutorial page

--- client/src/pages/Tutorial/index.jsx
+++ client/src/pages/Tutorial/index.jsx
@@ -16,40 +16,39 @@ class Tutorial extends Component {
             <h1 className="headline">Video Tutorial</h1>
             <div className="headline">
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/rQTtZSnJeoc" frameBorder="0"
-                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen></iframe>
+                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen/>
             </div>
             <h1 className="scrnmessage">Pollinate Home Page</h1>
             <div className="screenshot">
-                <img className="screenshot" src={HomePage}/>
+                <img alt='screenshot' className="screenshot" src={HomePage}/>
             </div>
             <h2 className="scrnmessage">Pollinate Category Page</h2>
             <div className="screenshot">
-                <img className="screenshot" src={CategoryPage}/>
+                <img alt='screenshot' className="screenshot" src={CategoryPage}/>
             </div>
             <h3 className="scrnmessage">Pollinate Item Page</h3>
             <div className="screenshot">
-                <img className="screenshot" src={ItemPage}/>
+                <img alt='screenshot' className="screenshot" src={ItemPage}/>
             </div>
             <h4 className="scrnmessage">Pollinate Login Page</h4>
             <div className="screenshot">
-                <img className="screenshot" src={LoginPage}/>
+                <img alt='screenshot' className="screenshot" src={LoginPage}/>
             </div>
             <h4 className="scrnmessage">Pollinate New Post Page</h4>
             <div className="screenshot">
-                <img className="signup" src={NewPostPage}/>
+                <img alt='screenshot' className="signup" src={NewPostPage}/>
             </div>
             <h5 className="scrnmessage">Pollinate Profile Page</h5>
             <div className="screenshot">
-                <img className="screenshot" src={ProfilePage}/>
+                <img alt='screenshot' className="screenshot" src={ProfilePage}/>
             </div>
             <h6 className="scrnmessage">Pollinate Search Page</h6>
             <div className="screenshot">
-                <img className="screenshot" src={SearchPage}/>
+                <img alt='screenshot' className="screenshot" src={SearchPage}/>
             </div>
             <h4 className="scrnmessage">Pollinate SignUp Page</h4>
             <div className="screenshot">
-                <img className="signup" src={SignUpPage}/>
+                <img alt='screenshot' className="signup" src={SignUpPage}/>
             </div>
         </div>
     )
