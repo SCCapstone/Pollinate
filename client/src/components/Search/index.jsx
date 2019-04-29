729cf56265d1b3686b09e1af7137760322a9e682
Bailey Metz <bametz23@gmail.com>
2019-04-29 19:34:06 -0400

    Comments added to Search component

--- client/src/components/Search/index.jsx
+++ client/src/components/Search/index.jsx
@@ -6,9 +6,16 @@ import {withRouter} from "react-router";
 class Search extends Component {
   navigate(e) {
     e.preventDefault();
+    //Performing the actual search function based on user entered keyword(s)
     this.props.history.push("/search?keyword=" + this.state.search);
   }
 
+
+
+  /*
+        This method displays the search bar and allows users to type character text
+        into the bar for indexing a specific item from items previously posted
+   */
   render() {
     return (
         <form className="form-inline my-2 my-lg-0 mr-3" onSubmit={e => this.navigate(e)}>
