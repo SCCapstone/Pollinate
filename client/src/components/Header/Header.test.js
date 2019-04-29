93cf72d49f8e03f83ce7959939e9d522e3e45382
Bailey Metz <bametz23@gmail.com>
2019-04-29 16:54:27 -0400

    Comments added to Header component

--- client/src/components/Header/Header.test.js
+++ client/src/components/Header/Header.test.js
@@ -3,6 +3,8 @@ import { shallow } from 'enzyme';
 import { Link } from 'react-router-dom';
 import Header from './';
 
+
+//This test ensures the Login button within the header navbar is clickable by users
 describe('Header', () => {
   it('Should render Link component with to prop of "/login"', () => {
     const component = shallow(<Header.WrappedComponent/>);
