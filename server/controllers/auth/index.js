fae1f35c141c204efcd5cfa094c44e9df2c42b8c
Michael Afrin <mkafrin@gmail.com>
2019-04-29 23:52:29 -0400

    Added additional comments to server controllers

--- server/controllers/auth/index.js
+++ server/controllers/auth/index.js
@@ -3,7 +3,7 @@ const crypto = require('crypto'),
     users = require('../users');
 
 exports.login = function (req, res) {
-  // Use req.body to get POST body. Check if user is in database and match password. Use res.send() to send error if
+  // Use req.body to get POST body. Check if user is in database and match password. Sends 401 status if
   // user doesn't exist/pass doesn't match, or send the non-sensitive user info
   const email = req.body.email;
   const password = req.body.password;
